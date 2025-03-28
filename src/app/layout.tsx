493d6b68b5b4da2741c28758e9850b974cb1d7c0
SubhamMohanta <subhammohanta99@gmail.com>
2025-03-28 10:01:43 +0530

    Added a vercel for tracking user actvity in the website

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import Navbar from "@/components/navbar";
 import { ThemeProvider } from "@/components/theme-provider";
 import { TooltipProvider } from "@/components/ui/tooltip";
+import { Analytics } from "@vercel/analytics/react"
 import { DATA } from "@/data/resume";
 import { cn } from "@/lib/utils";
 import type { Metadata } from "next";
